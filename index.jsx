a3d9a96fe45f4c4859ba76ae2f40604b73eccfb4
Guillaume VanderEst <guillaume@vanderest.org>
2017-06-26 14:30:34 -0700

    feat(index): exports

--- index.jsx
+++ index.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 let store = {};
 let dispatch = () => {};
 
-export Router;
-export Route;
-export Link;
-export store;
-export dispatch;
+export {
+    Router,
+    Route,
+    Link,
+    store,
+    dispatch
+};
