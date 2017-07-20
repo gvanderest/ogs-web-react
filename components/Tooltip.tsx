d91be3c7e393eaf189b9e7da3f36b3c71482ea5a
Guillaume VanderEst <guillaume@vanderest.org>
2017-07-20 13:40:22 -0700

    feat(Tooltip): convert to typescript

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React from "react";
 
 export default class Tooltip extends React.Component {
+    public static styled: boolean = true;
     constructor() {
         super();
         this.state = {
-            visible: false
+            visible: false,
         };
         this.element = null;
     }
-    shouldComponentUpdate(nextProps, nextState) {
+    public componentDidMount() {
+        this.refs.wrapper.onmouseover = this.show.bind(this);
+        this.refs.wrapper.onmouseout = this.hide.bind(this);
+        this.refs.wrapper.onclick = this.toggle.bind(this);
+    }
+    public shouldComponentUpdate(nextProps, nextState) {
         if (
             nextProps.text !== this.props.text ||
             nextProps.html !== this.props.html ||
@@ -21,22 +25,102 @@ export default class Tooltip extends React.Component {
         }
         return false;
     }
-    show() {
+    public componentDidUpdate() {
+        if (!this.state.visible) {
+            return;
+        }
+        const tooltipElement: HTMLElement = this.refs.tooltip;
+        if (!tooltip) {
+            return;
+        }
+        const wrapperElement: HTMLElement = this.refs.wrapper;
+
+        const wrapperBounding: any = wrapper.getBoundingClientRect();
+        const wrapperWidth = wrapperBounding.width;
+        const wrapperHeight = wrapperBounding.height;
+
+        const tooltipBounding = tooltip.getBoundingClientRect();
+        const tooltipWidth = tooltipBounding.width;
+        const tooltipHeight = tooltipBounding.height;
+
+        let [tooltipLeft, tooltipTop] = this.findPos(wrapper);
+
+        const position = this.props.position || "top";
+
+        const offsetLeft: number = 0;
+        const offsetTop: number = 0;
+
+        const padding = 10;
+        const bigger;
+        const smaller;
+        if (position === "top" || position === "bottom") {
+            bigger = Math.max(tooltipWidth, wrapperWidth);
+            smaller = Math.min(tooltipWidth, wrapperWidth);
+            offsetLeft -= ((bigger - smaller) / 2);
+
+            if (position === "top") {
+                offsetTop -= (tooltipHeight + padding);
+            } else if (position === "bottom") {
+                offsetTop += (wrapperHeight + padding);
+            }
+        } else if (position === "left" || position === "right") {
+            if (position === "left") {
+                offsetLeft -= tooltipWidth + 10;
+            } else if (position === "right") {
+                offsetLeft += wrapperWidth + 10;
+            }
+            bigger = Math.max(tooltipHeight, wrapperHeight);
+            smaller = Math.min(tooltipHeight, wrapperHeight);
+            offsetTop -= ((bigger - smaller) / 2);
+        }
+
+        // Ensure tooltip fits in the window
+        // let screenWidth = document.body.clientWidth;
+        // let additionalOffscreenLeft = -1 * Math.floor(Math.min(0, screenWidth -
+        //     (wrapperLeft + offsetLeft + tooltipWidth)));
+        // if (additionalOffscreenLeft > 0) {
+        //     offsetLeft -= additionalOffscreenLeft + 10;
+        // }
+
+        // Apply offsets and position tooltip
+        tooltipLeft += offsetLeft;
+        tooltipTop += offsetTop;
+
+        tooltip.style.left = `${tooltipLeft}px`;
+        tooltip.style.top = `${tooltipTop}px`;
+    }
+    public render() {
+        const position = this.props.position || "top";
+        return (
+            <span className="tooltip-wrapper" ref="wrapper">
+                { this.state.visible ? (
+                    <span className={ "tooltip-container " + ("on-" + position) } ref="tooltip">
+                        { this.props.html ? (
+                            <span
+                                className="tooltip-message"
+                                dangerouslySetInnerHTML={ { __html: this.props.html } }
+                            ></span>
+                        ) : (
+                            <span className="tooltip-message">{ this.props.text }</span>
+                        ) }
+                    </span>
+                ) : null }
+                { this.props.children }
+            </span>
+        );
+    }
+    protected show() {
         this.setState({ visible: true });
     }
-    hide() {
+    protected hide() {
         this.setState({ visible: false });
     }
-    toggle() {
+    protected toggle() {
         this.setState({ visible: !this.state.visible });
     }
-    componentDidMount() {
-        this.refs.wrapper.onmouseover = this.show.bind(this);
-        this.refs.wrapper.onmouseout = this.hide.bind(this);
-        this.refs.wrapper.onclick = this.toggle.bind(this);
-    }
-    findPos(obj) {
-        let curleft = 0, curtop = 0;
+    protected findPos(obj) {
+        const curleft = 0;
+        const curtop = 0;
         if (obj.offsetParent) {
             do {
                 curleft += obj.offsetLeft;
@@ -48,99 +132,12 @@ export default class Tooltip extends React.Component {
             return [curleft, curtop];
         }
     }
-    componentDidUpdate() {
-        if (!this.state.visible) {
-            return;
-        }
-        let tooltip = this.refs.tooltip;
-        if (!tooltip) {
-            return;
-        }
-        let wrapper = this.refs.wrapper;
-
-        let wrapperBounding = wrapper.getBoundingClientRect();
-        let wrapperWidth = wrapperBounding.width;
-        let wrapperHeight = wrapperBounding.height;
-
-        let tooltipBounding = tooltip.getBoundingClientRect();
-        let tooltipWidth = tooltipBounding.width;
-        let tooltipHeight = tooltipBounding.height;
-
-        let [tooltipLeft, tooltipTop] = this.findPos(wrapper);
-
-        let position = this.props.position || 'top';
-
-        let offsetLeft = 0, offsetTop = 0;
-
-        let padding = 10;
-        let bigger, smaller;
-        if (position === 'top' || position === 'bottom') {
-            bigger = Math.max(tooltipWidth, wrapperWidth);
-            smaller = Math.min(tooltipWidth, wrapperWidth);
-            offsetLeft -= ((bigger - smaller) / 2);
-
-            if (position === 'top') {
-                offsetTop -= (tooltipHeight + padding);
-            } else if (position === 'bottom') {
-                offsetTop += (wrapperHeight + padding);
-            }
-        } else if (position === 'left' || position === 'right') {
-            if (position === 'left') {
-                offsetLeft -= tooltipWidth + 10;
-            } else if (position === 'right') {
-                offsetLeft += wrapperWidth + 10;
-            }
-            bigger = Math.max(tooltipHeight, wrapperHeight);
-            smaller = Math.min(tooltipHeight, wrapperHeight);
-            offsetTop -= ((bigger - smaller) / 2);
-        }
-
-        // Ensure tooltip fits in the window
-        // let screenWidth = document.body.clientWidth;
-        // let additionalOffscreenLeft = -1 * Math.floor(Math.min(0, screenWidth - (wrapperLeft + offsetLeft + tooltipWidth)));
-        // if (additionalOffscreenLeft > 0) {
-        //     offsetLeft -= additionalOffscreenLeft + 10;
-        // }
-
-        // Apply offsets and position tooltip
-        tooltipLeft += offsetLeft;
-        tooltipTop += offsetTop;
-
-        tooltip.style.left = `${tooltipLeft}px`
-        tooltip.style.top = `${tooltipTop}px`
-    }
-    render() {
-        let position = this.props.position || 'top';
-        return (
-            <span className="tooltip-wrapper" ref="wrapper">
-                { this.state.visible ? (
-                    <span className={ 'tooltip-container ' + ('on-' + position) } ref="tooltip">
-                        { this.props.html ? (
-                            <span className="tooltip-message" dangerouslySetInnerHTML={ { __html: this.props.html } }></span>
-                        ) : (
-                            <span className="tooltip-message">{ this.props.text }</span>
-                        ) }
-                    </span>
-                ) : null }
-                { this.props.children }
-            </span>
-        );
-    }
 }
 
 Tooltip.propTypes = {
-    html: PropTypes.string,
-    text: PropTypes.string,
     htmlOrText: (props) => {
         if (!props.text && !props.html) {
-            throw new Error('Invalid prop: "message" or "text" prop must be provided');
+            throw new Error("Invalid prop: "message" or "text" prop must be provided");
         }
     },
-
-    position: PropTypes.string,
-
-    children: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.array,
-    ])
 };
