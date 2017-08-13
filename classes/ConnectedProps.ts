d92d1bf567c0c85ff154d1256a5905f17e4d2fe9
Guillaume VanderEst <guillaume@vanderest.org>
2017-08-12 21:08:25 -0700

    feat(ConnectedProps): add props for default actions/store reference

---
+++ classes/ConnectedProps.ts
@@ -0,0 +1,7 @@
+import ReduxActions from "./ReduxActions";
+import ReduxStore from "./ReduxStore";
+
+export default class ConnectedProps {
+    public actions: ReduxActions;
+    public store: ReduxStore;
+}
