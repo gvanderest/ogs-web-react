03b9f4e9b83c72d1edc5289a53cc8c43e49fa74c
Guillaume VanderEst <guillaume@vanderest.org>
2017-08-13 17:59:17 -0700

    feat(Entity): add some definition to Entity

--- classes/Entity.ts
+++ classes/Entity.ts
@@ -1,5 +1,9 @@
-export default class Entity<T> {
-    public constructor(data: T) {
+interface IBaseEntity {
+    [key: string]: any;
+}
+
+export default class Entity<T extends IBaseEntity> {
+    public constructor(data?: T) {
         if (data) {
             for (const key of Object.keys(data)) {
                 this[key] = data[key];
