37fa8321940d8fa0f6d4f946b53a558e1a5f6e69
Guillaume VanderEst <guillaume@vanderest.org>
2017-08-13 19:54:02 -0700

    feat(Entity): add signature for potential keys

--- classes/Entity.ts
+++ classes/Entity.ts
@@ -3,6 +3,7 @@ interface IBaseEntity {
 }
 
 export default class Entity<T extends IBaseEntity> {
+    [key: string]: any;
     public constructor(data?: T) {
         if (typeof data === "object") {
             for (const key of Object.keys(data)) {
